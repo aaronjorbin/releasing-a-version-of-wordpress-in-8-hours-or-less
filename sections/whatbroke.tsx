2de1f03184058eaa065ded20cb91db22bfe53761
Aaron Jorbin <aaronjorbin@users.noreply.github.com>
2024-07-31 13:55:08 -0500

    Fix lint

--- sections/whatbroke.tsx
+++ sections/whatbroke.tsx
@@ -1,40 +1,53 @@
-import { Slide, FlexBox, Heading, Notes, Stepper  } from 'spectacle';
+import { Slide, FlexBox, Heading, Notes, Stepper } from 'spectacle';
 
 const Whatbroke = () => (
 	<>
 		<Slide>
 			<FlexBox height="100%">
-				<Stepper tagName="h1" alwaysVisible values={
-					[ 'cURL', 
-					  'Client for URL',
-					  'Started in 1998',
-					  'Over twenty billion installations',
-					  'Releases about every 39 days'
-					]
-					}>
+				<Stepper
+					tagName="h1"
+					alwaysVisible
+					values={ [
+						'cURL',
+						'Client for URL',
+						'Started in 1998',
+						'Over twenty billion installations',
+						'Releases about every 39 days',
+					] }
+				>
 					{ ( value ) =>
-						value ? 
-							<Heading>{ value }</Heading> :
-						<Heading>What Broke?</Heading>
+						value ? (
+							<Heading>{ value }</Heading>
+						) : (
+							<Heading>What Broke?</Heading>
+						)
 					}
 				</Stepper>
 			</FlexBox>
-			<Notes>You might have heard of this being a problem with Curl, but what is CURL?</Notes>
+			<Notes>
+				You might have heard of this being a problem with Curl, but what
+				is CURL?
+			</Notes>
 		</Slide>
 		<Slide>
 			<FlexBox height="100%">
-				<Stepper tagName="h1" alwaysVisible values={
-					[ 'Started by Ryan McCue', 
-					  'A part of the WordPress project',
-					  'Used outside of just WordPress',
-					  'Why?',
-					  "You can't rely on cURL being everywhere"
-					]
-					}>
+				<Stepper
+					tagName="h1"
+					alwaysVisible
+					values={ [
+						'Started by Ryan McCue',
+						'A part of the WordPress project',
+						'Used outside of just WordPress',
+						'Why?',
+						"You can't rely on cURL being everywhere",
+					] }
+				>
 					{ ( value ) =>
-						value ? 
-							<Heading>{ value }</Heading> :
-						<Heading>WordPress Core Uses Requests</Heading>
+						value ? (
+							<Heading>{ value }</Heading>
+						) : (
+							<Heading>WordPress Core Uses Requests</Heading>
+						)
 					}
 				</Stepper>
 			</FlexBox>
