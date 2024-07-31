a8b0f5980d8b0f33952116036128411458b4799b
Aaron Jorbin <aaronjorbin@users.noreply.github.com>
2024-07-31 13:26:24 -0500

    Start outlining cURL and Requests

--- sections/whatbroke.tsx
+++ sections/whatbroke.tsx
@@ -1,14 +1,42 @@
-import { Slide, FlexBox, Heading, Notes } from 'spectacle';
+import { Slide, FlexBox, Heading, Notes, Stepper  } from 'spectacle';
 
 const Whatbroke = () => (
 	<>
 		<Slide>
-			<FlexBox height="100%">Make Slide with clock</FlexBox>
-			<Notes>Release at XX:XX</Notes>
+			<FlexBox height="100%">
+				<Stepper tagName="h1" alwaysVisible values={
+					[ 'cURL', 
+					  'Client for URL',
+					  'Started in 1998',
+					  'Over twenty billion installations',
+					  'Releases about every 39 days'
+					]
+					}>
+					{ ( value ) =>
+						value ? 
+							<Heading>{ value }</Heading> :
+						<Heading>What Broke?</Heading>
+					}
+				</Stepper>
+			</FlexBox>
+			<Notes>You might have heard of this being a problem with Curl, but what is CURL?</Notes>
 		</Slide>
 		<Slide>
 			<FlexBox height="100%">
-				<Heading>Thanks Crowdstrike!</Heading>
+				<Stepper tagName="h1" alwaysVisible values={
+					[ 'Started by Ryan McCue', 
+					  'A part of the WordPress project',
+					  'Used outside of just WordPress',
+					  'Why?',
+					  "You can't rely on cURL being everywhere"
+					]
+					}>
+					{ ( value ) =>
+						value ? 
+							<Heading>{ value }</Heading> :
+						<Heading>WordPress Core Uses Requests</Heading>
+					}
+				</Stepper>
 			</FlexBox>
 		</Slide>
 	</>
