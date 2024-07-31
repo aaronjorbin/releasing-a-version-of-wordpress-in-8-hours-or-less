3e6f6b633090bd9802342ae37c0a8a925ad43adb
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2024-07-31 14:15:42 -0400

    Fix linting errors.

--- sections/behind.tsx
+++ sections/behind.tsx
@@ -1,4 +1,4 @@
-import { Slide, FlexBox, Heading, Text, Quote, Notes } from 'spectacle';
+import { Slide, FlexBox, Heading, Text, Notes } from 'spectacle';
 
 const Behind = () => (
 	<>
@@ -10,11 +10,8 @@ const Behind = () => (
 			<Notes>Release at XX:XX</Notes>
 		</Slide>
 
-		<Slide
-			backgroundImage="url(images/default-bsod.png)"
-		>
-			<FlexBox height="100%">
-			</FlexBox>
+		<Slide backgroundImage="url(images/default-bsod.png)">
+			<FlexBox height="100%"></FlexBox>
 		</Slide>
 	</>
 );
