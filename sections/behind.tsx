42c6e648dd71aeed375246cfe9ad7b3297a2ec0f
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2024-08-05 11:06:02 -0400

    Add section about release types.

--- sections/behind.tsx
+++ sections/behind.tsx
@@ -1,4 +1,4 @@
-import { Slide, FlexBox, Heading, Text, Notes } from 'spectacle';
+import { Slide, FlexBox, Heading, Text, Notes, UnorderedList, ListItem } from 'spectacle';
 
 // @ts-ignore
 import imgUrl from  '../images/default-bsod.png' ;
@@ -8,7 +8,41 @@ const Behind = () => (
 		<Slide>
 			<FlexBox height="100%" flexDirection="column">
 				<Heading textAlign="center">Behind the curtain</Heading>
-				<Text>What goes on behind the scenes?</Text>
+				<Text>How does a release happen?</Text>
+			</FlexBox>
+			<Notes>Release at XX:XX</Notes>
+		</Slide>
+
+		<Slide>
+			<FlexBox height="100%" flexDirection="column">
+				<Heading textAlign="center">Release types</Heading>
+				<UnorderedList>
+					<ListItem>
+						Planned
+						<UnorderedList>
+							<ListItem>Major</ListItem>
+							<ListItem>Minor (Maintenance)</ListItem>
+							<ListItem>Minor (Security)</ListItem>
+							<ListItem>Minor (Maintenance & Security)</ListItem>
+						</UnorderedList>
+					</ListItem>
+				</UnorderedList>
+			</FlexBox>
+			<Notes>Release at XX:XX</Notes>
+		</Slide>
+
+		<Slide>
+			<FlexBox height="100%" flexDirection="column">
+				<Heading textAlign="center">Release types</Heading>
+				<UnorderedList>
+					<ListItem>
+						Unplanned
+						<UnorderedList>
+							<ListItem>Minor (Security)</ListItem>
+							<ListItem>Minor (Something is broken)</ListItem>
+						</UnorderedList>
+					</ListItem>
+				</UnorderedList>
 			</FlexBox>
 			<Notes>Release at XX:XX</Notes>
 		</Slide>
