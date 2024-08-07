87af83906c72784552ef21e9b9f0d6b53e3c6508
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2024-08-07 14:15:38 -0400

    Apply formatting fixes.

--- sections/how-it-happens.tsx
+++ sections/how-it-happens.tsx
@@ -7,40 +7,37 @@ import xLogoImg from '../images/x-logo-black.png';
 const Howithappens = () => (
 	<>
 		<Slide>
-			<FlexBox height="100%" >
-				<Heading>
-					How does a release happen?
-				</Heading>
+			<FlexBox height="100%">
+				<Heading>How does a release happen?</Heading>
 			</FlexBox>
 			<Notes>Many moving parts.</Notes>
 		</Slide>
 
 		<Slide>
-			<FlexBox height="100%" >
-				<Heading fontSize="400px">
-					👂
-				</Heading>
+			<FlexBox height="100%">
+				<Heading fontSize="400px">👂</Heading>
 			</FlexBox>
-			<Notes>Feedback loops. How do we look and listen for problems that a response may be needed?</Notes>
+			<Notes>
+				Feedback loops. How do we look and listen for problems that a
+				response may be needed?
+			</Notes>
 		</Slide>
 
 		<Slide backgroundImage={ 'url(' + forumsImg + ')' }>
+			<FlexBox height="100%"></FlexBox>
+		</Slide>
+
+		<Slide>
 			<FlexBox height="100%">
+				<Image src={ hostingImg } width={ '65%' }></Image>
 			</FlexBox>
 		</Slide>
 
 		<Slide>
 			<FlexBox height="100%">
-				<Image src={ hostingImg } width={ "65%" }></Image>
+				<Image src={ xLogoImg } width={ '30%' }></Image>
 			</FlexBox>
 		</Slide>
-
-		<Slide>
-			<FlexBox height="100%">
-				<Image src={ xLogoImg } width={ "30%" }></Image>
-			</FlexBox>
-		</Slide>
-
 	</>
 );
 
