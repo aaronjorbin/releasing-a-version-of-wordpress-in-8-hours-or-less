8f20caf7d7f5a832f1d61d8f57be50a8d25c5059
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2024-08-07 14:12:13 -0400

    Add slides about feedback loops.

--- sections/how-it-happens.tsx
+++ sections/how-it-happens.tsx
@@ -1,16 +1,46 @@
-import { Slide, FlexBox, Heading, Notes } from 'spectacle';
+import { Slide, FlexBox, Heading, Notes, Image } from 'spectacle';
+
+import forumsImg from '../images/w.org-forums.png';
+import hostingImg from '../images/make-hosting.png';
+import xLogoImg from '../images/x-logo-black.png';
 
 const Howithappens = () => (
 	<>
 		<Slide>
-			<FlexBox height="100%">Make Slide with clock</FlexBox>
-			<Notes>Release at XX:XX</Notes>
+			<FlexBox height="100%" >
+				<Heading>
+					How does a release happen?
+				</Heading>
+			</FlexBox>
+			<Notes>Many moving parts.</Notes>
 		</Slide>
+
 		<Slide>
+			<FlexBox height="100%" >
+				<Heading fontSize="400px">
+					👂
+				</Heading>
+			</FlexBox>
+			<Notes>Feedback loops. How do we look and listen for problems that a response may be needed?</Notes>
+		</Slide>
+
+		<Slide backgroundImage={ 'url(' + forumsImg + ')' }>
 			<FlexBox height="100%">
-				<Heading>Thanks Crowdstrike!</Heading>
 			</FlexBox>
 		</Slide>
+
+		<Slide>
+			<FlexBox height="100%">
+				<Image src={ hostingImg } width={ "65%" }></Image>
+			</FlexBox>
+		</Slide>
+
+		<Slide>
+			<FlexBox height="100%">
+				<Image src={ xLogoImg } width={ "30%" }></Image>
+			</FlexBox>
+		</Slide>
+
 	</>
 );
 
