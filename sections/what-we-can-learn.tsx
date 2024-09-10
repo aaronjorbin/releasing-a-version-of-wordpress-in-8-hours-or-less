dd9508ef98020375bbf8951e90a3ad9faaef6d8b
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2024-09-10 13:43:48 -0400

    Adjust some lessons learned.

--- sections/what-we-can-learn.tsx
+++ sections/what-we-can-learn.tsx
@@ -9,34 +9,14 @@ const Whatwecanlearn = () => (
 			<FlexBox height="100%">
 				<Heading>Lessons for WordPress Core</Heading>
 				<Notes>
-					You need to test The yankees are a terrible franchise
-					Historical Context only helps so much (we live in
-					unprecedented times) Divide and Conquer (if it can happen in
-					parallel, have it happen in parallel) Take breaks, drink
-					water, walk dogs, feed families.
+					You need to test Historical Context only helps so much (we
+					live in unprecedented times) Divide and Conquer (if it can
+					happen in parallel, have it happen in parallel) Take breaks,
+					drink water, walk dogs, feed families.
 				</Notes>
 			</FlexBox>
 		</Slide>
-		<Slide>
-			<FlexBox height="100%">
-				<Heading>The Reliability of Updates, depends on you!</Heading>
-				<Notes>
-					This is especially true for Hosting providores. Ask your
-					host how they are getting ready for new versions of
-					WordPress and if the answer does not involve them testing,
-					go find a new host.
-				</Notes>
-			</FlexBox>
-		</Slide>
-		<Slide backgroundImage={ 'url(' + imgUrl + ')' }>
-			<FlexBox height="100%"></FlexBox>
-			<Notes>
-				If your site broke during the upgrade to 6.4 and you do not see
-				your host on this list, ask them why. If you are a host and do
-				not see yourself on this list, ask in the #hosting channel and
-				get yourself added.
-			</Notes>
-		</Slide>
+
 		<Slide>
 			<FlexBox height="100%">
 				<Heading>Process Documenation is a Blueprint</Heading>
@@ -58,6 +38,36 @@ const Whatwecanlearn = () => (
 					and allowed one additional bug to be fixed.
 				</Notes>
 			</FlexBox>
+		</Slide>
+		<Slide>
+			<FlexBox height="100%">
+				<Heading>The Reliability of Updates, depends on you!</Heading>
+				<Notes>
+					This is especially true for Hosting providores. Ask your
+					host how they are getting ready for new versions of
+					WordPress and if the answer does not involve them testing,
+					go find a new host.
+				</Notes>
+			</FlexBox>
+		</Slide>
+
+		<Slide backgroundImage={ 'url(' + imgUrl + ')' }>
+			<FlexBox height="100%"></FlexBox>
+			<Notes>
+				If your site broke during the upgrade to 6.4 and you do not see
+				your host on this list, ask them why. If you are a host and do
+				not see yourself on this list, ask in the #hosting channel and
+				get yourself added.
+			</Notes>
+		</Slide>
+
+		<Slide backgroundImage={ 'url(' + imgUrl + ')' }>
+			<FlexBox height="100%"></FlexBox>
+			<Notes>
+				Now supports multiple reports per commit to allow for multiple
+				environment types, multiple PHP or database versions, operating
+				systems, product levels, etc..
+			</Notes>
 		</Slide>
 	</>
 );
