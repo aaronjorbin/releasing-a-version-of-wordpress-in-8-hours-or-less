dff759ebccb64e5516c5f3795c592f1c0a3d9484
Aaron Jorbin <aaronjorbin@users.noreply.github.com>
2024-09-12 14:45:39 -0700

    Reformat tiktok section

--- components/TikTokSlide.tsx
+++ components/TikTokSlide.tsx
@@ -1,29 +1,145 @@
 import { Slide, Grid, Heading, Notes, FlexBox } from 'spectacle';
 
-const TikTokSlide = ( { text, time, date, notes }: SlideProps ) => (
-	<Slide>
-		<Grid
-			gridColumnGap={ 2 }
-			gridTemplateColumns={ 'repeat(2, 1fr)' }
-			height={ '100%' }
-		>
-			<FlexBox height="100%" flexDirection="column">
-				<Heading>{ date }</Heading>
-				<Heading>{ time }</Heading>
+const timeSinceRelease = ( date: string, time: string ) => {
+	return 'Time Since Release: ' + TimeDiff( new Date( `${ date } ${ time }` ), new Date( 'November 7, 2023 20:00' ) );
+}
+
+const timePreparingRelease = ( date: string, time: string ) => {
+	return 'Time Preparing Release: ' + TimeDiff( new Date( 'November 8, 2023 18:05' ), new Date( `${ date } ${ time }` ),  );
+}
+
+const transition = {
+	from: {
+	},
+	enter: {
+	},
+	leave: {
+	}
+  };
+
+const TikTokSlide = ( { text, time, date, notes, since }: SlideProps ) => (
+	<Slide transition={transition}>
+		<FlexBox height="100%" flexDirection="column" justifyContent="space-between">
+			<FlexBox flexDirection="row" font>
+				<Heading fontSize="text" color="primary">{ date }</Heading>
+				<Heading fontSize="text" color="primary">{ time }</Heading>
 			</FlexBox>
-			<FlexBox height="100%" flexDirection="column">
+			<FlexBox flexDirection="column">
 				<Heading>{ text }</Heading>
 			</FlexBox>
-		</Grid>
+			<FlexBox flexDirection="column">
+				<Heading fontSize="text" color="primary">
+					{ since ? timeSinceRelease( date, time ) : timePreparingRelease( date, time ) }
+				</Heading>
+			</FlexBox>
+		</FlexBox>
 		<Notes>{ notes }</Notes>
 	</Slide>
 );
+
+
+/**
+ * calculate the time difference
+ * @param {Date} startTime
+ * @param {Date} endTime
+ * @returns {Error|string}
+ * @constructor
+ */
+const TimeDiff = ( startTime: Date, endTime:Date ) => {
+
+    startTime = new Date( startTime );
+
+    endTime = endTime ? new Date( endTime ) : new Date();
+
+    const startTimeInMS = startTime.getTime();
+
+    const endTimeInMS = endTime.getTime();
+
+    if( !startTimeInMS ) return new Error('Invalid start date');
+
+    if( !endTimeInMS ) return new Error('Invalid end date');
+
+    /**
+     * time difference in milliseconds
+     * @type {number}
+     */
+    let timeDifference = endTimeInMS - startTimeInMS;
+
+    const tense = timeDifference > 0 ? 'ago' : 'after';
+
+    timeDifference = timeDifference > 0 ? timeDifference : - timeDifference;
+
+    /**
+     * time difference in seconds
+     * @type {number}
+     */
+    const secondsAgo = timeDifference/1000;
+
+    /**
+     * time difference in minutes
+     * @type {number}
+     */
+    const minutesAgo = secondsAgo/60;
+
+    /**
+     * time difference in hours
+     * @type {number}
+     */
+    const hoursAgo = minutesAgo/60;
+
+    /**
+     * time difference in days
+     * @type {number}
+     */
+    const daysAgo = hoursAgo/24;
+
+	const value = [];
+
+    if( hoursAgo >= 24 ){
+        const day = Math.floor( daysAgo );
+        if( day === 1 ){
+            value.push( `${day} day` );
+        }
+        else{
+            value.push( `${day} days` );
+        }
+    }
+    if( minutesAgo >= 60 ){
+        const hour = Math.floor( hoursAgo ) - ( Math.floor( daysAgo ) * 24 );
+		if ( hour <= 0 ){
+			
+		}
+        else if( hour === 1 ){
+            value.push(  `${hour} hour` );
+        }
+        else{
+            value.push( `${hour} hours` );
+        }
+    }
+    if( secondsAgo >= 60 ){
+        const minute = Math.floor( minutesAgo ) - ( Math.floor( hoursAgo ) * 60 );
+        if( minute === 1 ){
+            value.push( `${minute} minute` );
+        }
+        else{
+            value.push( `${minute} minutes` );
+        }
+    }
+	console.log( value );
+	if ( value.length === 0 ){
+		return 'None';
+	}
+	return value.join( ', ' );
+
+};
+
 
 type SlideProps = {
 	text: string;
 	time: string;
 	date: string;
 	notes?: string;
+	since: boolean;
 };
 
 export default TikTokSlide;
--- sections/tiktok.tsx
+++ sections/tiktok.tsx
@@ -6,83 +6,100 @@ const TikTok = () => (
 			text="6.4 is officially released"
 			time="20:00"
 			date="November 7, 2023"
+			since={ true }
 		/>
 		<TikTokSlide
 			text="Issue is first reported to Requests"
 			time="10:53"
 			date="November 8, 2023"
+			since={ true }
 		/>
 		<TikTokSlide
 			text="Issue is first reported in Support Forums"
 			time="13:18"
 			date="November 8, 2023"
+			since={ true }
 		/>
 		<TikTokSlide
 			text="Issue is first reported to WordPress Core"
 			time="16:22"
 			date="November 8, 2023"
+			since={ true }
 		/>
 		<TikTokSlide
 			text="Issue is first raised in slack"
 			time="18:05"
 			date="November 8, 2023"
+			since={ true }
+			notes="This is the moment where the possibility of a release is first considered"
 		/>
 		<TikTokSlide
 			text="Problomatic Code is reverted in Requests"
 			time="18:13"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="Division of work and plan for potential release"
 			time="19:07"
 			date="November 8, 2023"
 			notes="We move the specific issue to a seperate room and scrub every issue/report"
+			since={ false }
 		/>
-		<TikTokSlide text="Scrub begins" time="19:11" date="November 8, 2023" />
+		<TikTokSlide text="Scrub begins" time="19:11" date="November 8, 2023" since={ false } />
 		<TikTokSlide
 			text="Updates to 6.4 are paused and wp.org is made immune from the issue"
 			time="19:12"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="Requests 2.0.9 is released"
 			time="19:34"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="Requests 2.0.9 is merged into WordPress Trunk"
 			time="19:46"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="Exact conditions of the issue are identified and published"
 			time="20:08"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="Fix for bug in wp_admin_notice is backported to 6.4"
 			time="20:08"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="Fix for bug in wpdb is backported to 6.4"
 			time="20:36"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="WordPress 6.4.1 RC1 is released for testing."
 			time="21:29"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="Fix for bug in bulk edit is backported to 6.4"
 			time="23:20"
 			date="November 8, 2023"
+			since={ false }
 		/>
 		<TikTokSlide
 			text="WordPress 6.4.1 is released"
 			time="01:26"
 			date="November 9, 2023"
+			since={ false }
 		/>
 	</>
 );
