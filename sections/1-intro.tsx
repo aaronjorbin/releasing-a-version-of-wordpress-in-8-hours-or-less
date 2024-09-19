3a0654fba02b33b538bb3e1990291c717f7f960d
Aaron Jorbin <aaronjorbin@users.noreply.github.com>
2024-09-18 17:57:51 -0700

    Small Changes

--- sections/1-intro.tsx
+++ sections/1-intro.tsx
@@ -1,4 +1,4 @@
-import { Slide, FlexBox, Image, Heading } from 'spectacle';
+import { Slide, FlexBox, Image, Heading, Notes } from 'spectacle';
 
 // @ts-ignore
 import tweetImg from '../images/francesca-tweet.png';
@@ -10,7 +10,7 @@ const Intro = () => (
 		<Slide>
 			<FlexBox height="100%">
 				<Heading>
-					Releasing a Version of WordPress in 8 hours or less
+					Releasing a Version of WordPress in 8 Hours or Less
 				</Heading>
 			</FlexBox>
 		</Slide>
@@ -18,11 +18,17 @@ const Intro = () => (
 			<FlexBox height="100%">
 				<Image src={ tweetImg } />
 			</FlexBox>
+			<Notes>
+				The day after the release of WordPress 6.4, one of the Release
+				Coodinators, Francesca Marano, shared this tweet. And she&apos;s
+				right, but also...
+			</Notes>
 		</Slide>
 		<Slide>
 			<FlexBox height="100%">
 				<Image src={ jorbinImg } />
 			</FlexBox>
+			<Notes>Oh how little did she know at this moment.</Notes>
 		</Slide>
 	</>
 );
