054e68969df5efb7600bc76778ed479e56ff4976
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2024-09-20 09:56:32 -0700

    Add speaker queues to host section and update image.

--- sections/9-hosting-tests.tsx
+++ sections/9-hosting-tests.tsx
@@ -10,10 +10,10 @@ const Hostingtests = () => (
 			<FlexBox height="100%">
 				<Heading>The Reliability of Updates, depends on you!</Heading>
 				<Notes>
-					This is especially true for Hosting providers. Ask your host
-					how they are getting ready for new versions of WordPress and
-					if the answer does not involve them testing, go find a new
-					host.
+					Jorbin: This is especially true for Hosting providers. Ask
+					your host how they are getting ready for new versions of
+					WordPress and if the answer does not involve them testing,
+					go find a new host.
 				</Notes>
 			</FlexBox>
 		</Slide>
@@ -21,10 +21,10 @@ const Hostingtests = () => (
 		<Slide backgroundImage={ 'url(' + imgUrl + ')' }>
 			<FlexBox height="100%"></FlexBox>
 			<Notes>
-				If your site broke during the upgrade to 6.4 and you do not see
-				your host on this list, ask them why. If you are a host and do
-				not see yourself on this list, ask in the #hosting channel and
-				get yourself added.
+				Jorbin: If your site broke during the upgrade to 6.4 and you do
+				not see your host on this list, ask them why. If you are a host
+				and do not see yourself on this list, ask in the #hosting
+				channel and get yourself added.
 			</Notes>
 		</Slide>
 
@@ -33,9 +33,9 @@ const Hostingtests = () => (
 				<Image src={ multiImg } width={ '65%' }></Image>
 			</FlexBox>
 			<Notes>
-				Now supports multiple reports per commit to allow for multiple
-				environment types, multiple PHP or database versions, operating
-				systems, product levels, etc..
+				Jon: Now supports multiple reports per commit to allow for
+				multiple environment types, multiple PHP or database versions,
+				operating systems, product levels, etc..
 			</Notes>
 		</Slide>
 	</>
