bc15df8276a5c1b2a7033f6c3654dec96369a3d4
Aaron Jorbin <aaronjorbin@users.noreply.github.com>
2024-09-20 10:15:04 -0700

    It's the final countdown

--- sections/5-whatbroke.tsx
+++ sections/5-whatbroke.tsx
@@ -9,29 +9,30 @@ const Whatbroke = () => (
 			<FlexBox height="100%">
 				<Heading>What Broke?</Heading>
 			</FlexBox>
-			<Notes>Nothing actually broke. The unit tests passed.</Notes>
+			<Notes>Jon: Nothing actually broke. The unit tests passed.</Notes>
 		</Slide>
 		<Slide backgroundImage={ 'url(' + changeset + ')' }>
 			<FlexBox height="100%"></FlexBox>
 			<Notes>
-				Innocent enough, right? Merged to Requests on 17 August, 2023
-				Synced to Core on 11 September 2023 Available in every beta and
-				RC for 6.4
+				Jon: Innocent enough, right? Merged to Requests on 17 August,
+				2023 Synced to Core on 11 September 2023 Available in every beta
+				and RC for 6.4
 			</Notes>
 		</Slide>
 		<Slide>
 			<FlexBox height="100%">
 				<Heading>What Broke?</Heading>
 			</FlexBox>
-			<Notes>So what broke?</Notes>
+			<Notes>Jorbin: So what broke?</Notes>
 		</Slide>
 		<Slide>
 			<FlexBox height="100%">
 				<Heading>Nothing... But also everything</Heading>
 			</FlexBox>
 			<Notes>
-				For WordPress 6.4 There were four betas with three release
-				candidates. During that time 64 bugs were reported and fixed.
+				Jorbin: For WordPress 6.4 There were four betas with three
+				release candidates. During that time 64 bugs were reported and
+				fixed.
 			</Notes>
 		</Slide>
 
@@ -40,8 +41,8 @@ const Whatbroke = () => (
 				<Heading>What Broke?</Heading>
 			</FlexBox>
 			<Notes>
-				So what broke? To answer that, we need to look at very ubiqutous
-				piece of software.
+				Jorbin: So what broke? To answer that, we need to look at very
+				ubiqutous piece of software.
 			</Notes>
 		</Slide>
 
@@ -50,11 +51,10 @@ const Whatbroke = () => (
 				<Heading>cURL</Heading>
 			</FlexBox>
 			<Notes>
-				Client for URL. It is EVERYWHERE. Releases on average every 39
-				days, that&apos;s 9-10 releases per year. It&apos;s been
-				releasing that often since 1996. So it&apos;s something that is
-				older than some folks in the audience (7 years older than
-				WordPress).
+				Jorbin: Client for URL. Releases on average every 39 days,
+				that&apos;s 9-10 releases per year. It&apos;s been releasing
+				that often since 1996. So it&apos;s something that is older than
+				some folks in the audience (7 years older than WordPress).
 			</Notes>
 		</Slide>
 		<Slide>
@@ -62,7 +62,7 @@ const Whatbroke = () => (
 				<Heading>20B+ installations</Heading>
 			</FlexBox>
 			<Notes>
-				cURL is running on your phone and quite possibly on your
+				Jon: cURL is running on your phone and quite possibly on your
 				refregiatro (but if it is, you should try to catch it). cURL
 				also doesn&apos;t backport changes including security fixes but
 				many distribtions backport them.
@@ -74,7 +74,8 @@ const Whatbroke = () => (
 				<Heading>Requests</Heading>
 			</FlexBox>
 			<Notes>
-				A part of the WordPress project Used outside of just WordPress.
+				Jorbin: A part of the WordPress project Used outside of just
+				WordPress.
 			</Notes>
 		</Slide>
 		<Slide>
@@ -82,10 +83,10 @@ const Whatbroke = () => (
 				<Heading>You can&apos;t rely on cURL being everywhere</Heading>
 			</FlexBox>
 			<Notes>
-				This is needed since while cURL is everywhere, it is not
+				Jorbin: This is needed since while cURL is everywhere, it is not
 				actually everywhere, and may not be properly configured.
 				WordPress has a philosophy to work. cURL has an interesting API,
-				to say the least.
+				to say the least. Kick to Jon at end
 			</Notes>
 		</Slide>
 		<Slide>
@@ -93,9 +94,9 @@ const Whatbroke = () => (
 				<Heading>Out of the box</Heading>
 			</FlexBox>
 			<Notes>
-				We ask for as few technical details as possible during the setup
-				process. The Requirements are kept as minimal as possible which
-				means cURL is not required tfor WordPress to work.
+				Jon: We ask for as few technical details as possible during the
+				setup process. The Requirements are kept as minimal as possible
+				which means cURL is not required tfor WordPress to work.
 			</Notes>
 		</Slide>
 		<Slide>
@@ -110,16 +111,18 @@ const Whatbroke = () => (
 			items={ [
 				<>
 					<Notes>
-						Under certain circumstances, websites could not complete
-						most outbound HTTP requests. This means requests to
-						things like payment providers for ecommerce sites,
-						requests to embed content using oembed, but more
-						importantly it means some sites could no longer reach
-						WordPress.org to requests updates. WordPress.org
+						Jorbin first 2, Jon last 2. Under certain circumstances,
+						websites could not complete most outbound HTTP requests.
+						This means requests to things like payment providers for
+						ecommerce sites, requests to embed content using oembed,
+						but more importantly it means some sites could no longer
+						reach WordPress.org to requests updates. WordPress.org
 						temporarily disabled keep-alive for HTTP 1.1. HTTP 2.0
-						is unaffected (no support for keep-alive). Version 7.47
-						of cURL was released in 2015 during the same month as
-						WordPress 4.4. 20 versions of WordPress has passed.
+						is unaffected (no support for keep-alive). Version 7.22
+						was releasedo in Sep 2011 and 7.47 was released in 2015
+						during the same month as WordPress 4.4. 20 versions of
+						WordPress has passed but many sites hadn&apos;t updated
+						cURL.
 					</Notes>
 					cURL: must use HTTP 1
 				</>,
--- sections/7-how-it-happens.tsx
+++ sections/7-how-it-happens.tsx
@@ -14,7 +14,7 @@ const Howithappens = () => (
 				<Heading>How does a release happen?</Heading>
 			</FlexBox>
 			<Notes>
-				Many moving parts. No one person decides there should be a
+				Jon: Many moving parts. No one person decides there should be a
 				release. Each situation is different.
 			</Notes>
 		</Slide>
@@ -24,18 +24,22 @@ const Howithappens = () => (
 				<Heading fontSize="400px">👂</Heading>
 			</FlexBox>
 			<Notes>
-				Feedback loops. We look and listen for problems and areas where
-				a response may be needed.
+				Jorbin: Feedback loops. We look and listen for problems and
+				areas where a response may be needed.
 			</Notes>
 		</Slide>
 
 		<Slide backgroundImage={ 'url(' + forumsImg + ')' }>
 			<FlexBox height="100%"></FlexBox>
+			<Notes>Jorbin:</Notes>
 		</Slide>
 
 		<Slide>
 			<FlexBox height="100%">
 				<Image src={ hostingImg } width={ '65%' }></Image>
+				<Notes>
+					Jorbin: end with &quot;And we go looking for feedback&quot;
+				</Notes>
 			</FlexBox>
 		</Slide>
 
@@ -44,7 +48,7 @@ const Howithappens = () => (
 				<Image src={ svenImg } width={ '90%' }></Image>
 			</FlexBox>
 			<Notes>
-				Update Fatigue is a real problem. We need to balance the
+				Jon: Update Fatigue is a real problem. We need to balance the
 				benefits of the update with the pain of the bugs that are known.
 			</Notes>
 		</Slide>
@@ -54,13 +58,10 @@ const Howithappens = () => (
 				<Heading>The vocal minority</Heading>
 			</FlexBox>
 			<Notes>
-				&quot;The number of people who create content on the internet
-				represents approximately 1% (or less) of the people actually
-				viewing that content&quot; Engage with more of the non-vocal
-				users. Make measured decisions. It&apos;s also not an insult to
-				be in the vocal minority. We have both been in that group at
-				various times. For 6.4.1, there was enough feedback that showed
-				this was not just a case of vocal minority.
+				Jon: &quot;The number of people who create content on the
+				internet represents approximately 1% (or less) of the people
+				actually viewing that content&quot; Engage with more of the
+				non-vocal users. Make measured decisions.
 			</Notes>
 		</Slide>
 		<Slide>
@@ -68,9 +69,9 @@ const Howithappens = () => (
 				<Heading>Core Decision Making</Heading>
 			</FlexBox>
 			<Notes>
-				The core team engages in consensus seeking to make decisions.
-				That means we try to seek out all opinions and find the best
-				ones that lack strong opposition.
+				Jorbin: The core team engages in consensus seaking to make
+				decisions. That means we try to seek out all opinions and find
+				the best ones that lack strong opposition.
 			</Notes>
 		</Slide>
 		<Slide>
@@ -78,12 +79,10 @@ const Howithappens = () => (
 				<Heading>Who Decides When to Release</Heading>
 			</FlexBox>
 			<Notes>
-				There is no one person who decides when to release. It is a team
-				decision. The release lead is the final decision maker. Matt is
-				rarely involved in the decision. the process takes place in
-				public and anyone can participate. In the case of 6.4.1, enough
-				contributors that showed up felt this was significant enough to
-				be addressed ASAP.
+				Jon: There is no one person who decides when to release. It is a
+				team decision. The release lead is the final decision maker.
+				Matt is rarely involved in the decision. the process takes place
+				in public and anyone can participate.
 			</Notes>
 		</Slide>
 		<Slide>
@@ -91,11 +90,9 @@ const Howithappens = () => (
 				<Heading>Minor Release Leads</Heading>
 			</FlexBox>
 			<Notes>
-				Minor Releases are led by a small group (usually 1-3 people) who
-				are experienced contributors to WordPress Core. This is
-				generally Core Committers who want to prioritize maintaince.
-				They make the final decision on what goes into the release and
-				when it is ready.
+				Jorbin: Minor Releases are led by a small group (usually 1-3
+				people) who are experienced contributors to WordPress Core. This
+				is generally Core Committers who want to prioritize maintaince.
 			</Notes>
 		</Slide>
 		<Slide>
@@ -105,10 +102,10 @@ const Howithappens = () => (
 				</Heading>
 			</FlexBox>
 			<Notes>
-				One of the most important ways to keep a site secure is to keep
-				it updated. In the instance of 6.4.1, the pain of a bug that
-				could block websites from updating was considered a security
-				concern.
+				Jon: One of the most important ways to keep a site secure is to
+				keep it updated. In the instance of 6.4.1, the pain of a bug
+				that could block websites from updating was considered a
+				security concern.
 			</Notes>
 		</Slide>
 		<Slide>
@@ -116,9 +113,9 @@ const Howithappens = () => (
 				<Heading>Only One Version is Supported</Heading>
 			</FlexBox>
 			<Notes>
-				While WordPress backports security fixes as a courtesy, only the
-				most recent version is supported. You should always be running
-				the latest version of WordPress.
+				Jorbin: While WordPress backports security fixes as a courtesy,
+				only the most recent version is supported. You should always be
+				running the latest version of WordPress.
 			</Notes>
 		</Slide>
 	</>
